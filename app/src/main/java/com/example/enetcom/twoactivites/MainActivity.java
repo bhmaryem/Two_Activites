c8d0704e754afe32d4b5afadcffb0cfddcca1000
POST2 <post2.prog2@enetcom.usf.tn>
2023-10-14 11:15:47 +0100

    second commit

--- app/src/main/java/com/example/enetcom/twoactivites/MainActivity.java
+++ app/src/main/java/com/example/enetcom/twoactivites/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.enetcom.twoactivites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+    public static final int TEXT_REQUEST = 1;
     private EditText mMessageEditText;
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
@@ -19,10 +24,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         String message = mMessageEditText.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+            }
+        }
     }
 }
